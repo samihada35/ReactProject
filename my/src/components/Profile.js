f07c9343ab89f3d82ac95d7700b9e208b0698931
Samihada35 <vitasant35@gmail.com>
2019-10-13 00:43:05 +0300

    styleMain
    верстка готова +\-

--- my/src/components/Profile.js
+++ my/src/components/Profile.js
@@ -4,25 +4,44 @@ import style from './Profile.module.css';
 const Profile = () => {
     return (
         <main className={style.content}>
-            <img className={style.headImg} src="https://zdnet2.cbsistatic.com/hub/i/2019/09/05/7c148e17-3f7e-4166-b755-324799ba0c7a/821e5f436179a5f90978a04703fb06ad/atanas-malamov-tpmav6c33de-unsplash.jpg" alt="img"></img>
-            <div>
+            <div className={style.headDivImg}>
+                <img className={style.headImg} src="http://glowingbluecore.com/wordpress/wp-content/uploads/gbc-css-img-fill-demo-wide-picture.jpg" alt="img"></img>
+            </div>
+            
+            <div className={style.profileTop}>
                 <img className={style.avatarImg} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQToA9s3Y6-r1AO4iK79QL6hpW_6mlFDevRGvbg7Y7nc_UG2coM" alt="avatar"></img>
+                <div className={style.name}>Alex Clare</div>
+                <div className={style.dateOfBirth}>14.12.1974</div>
+                <div className={style.city}>NY</div>
+                <div className={style.education}>NY state universcity, PhD of psychology</div>
+                <div className={style.site}>www.JratGovno.com</div>
             </div>
 
-            <div>
+            <div className={style.myPost}>
                 My Posts
+            </div>
+            
+            <div>
+                <div className={style.newPost}>
+                    <form name='comment' method='post' action='#'>
 
-                <div>
-                    New Post
+                        <textarea name='maincomment' className={style.textareaPost} maxLength='500'></textarea>
+                        <div>
+                            <input type='submit' className={style.input} value='post'/>
+                        </div>
+                    </form>
                 </div>
 
                 <div>
+
                     <div className={style.item}>
-                        Post 1
+                        <img className={`${style.avatarImg} ${style.avatarNews}`} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQToA9s3Y6-r1AO4iK79QL6hpW_6mlFDevRGvbg7Y7nc_UG2coM" alt="avatar"></img>
+                        <div className={style.postNews}>My New Post</div>
                     </div>
 
                     <div className={style.item}>
-                        Post2
+                        <img className={`${style.avatarImg} ${style.avatarNews}`} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQToA9s3Y6-r1AO4iK79QL6hpW_6mlFDevRGvbg7Y7nc_UG2coM" alt="avatar"></img>
+                        <div className={style.postNews}>My New Post2</div>
                     </div>
                 </div>
             </div>
