f6f58201d5cd3223de153734f1e17cd95b1afccd
Samihada35 <vitasant35@gmail.com>
2020-02-19 18:38:23 +0300

    Update NavBar
    use map method for making NavBar menu more cool!

--- my/src/components/Navbar/Navbar.js
+++ my/src/components/Navbar/Navbar.js
@@ -2,28 +2,24 @@ import React from "react";
 import style from "./Navbar.module.css";
 import { NavLink } from "react-router-dom";
 
+let NavLinksData =[
+  {link: "/my/src/components/Profile/Profile.js", name: "Profile"},
+  {link: "/my/src/components/Dialogs/Dialogs.js", name: "Messages"},
+  {link: "/my/src/components/News/News.j", name: "News"},
+  {link: "/my/src/components/Music/Music.js", name: "Music"},
+  {link: "/my/src/components/Settings/Settings.js", name: "Settings"}
+];
+
+let NavLinks = NavLinksData.map(linkUrl => (
+  <div className={`${style.item} ${style.active}`}>
+    <NavLink to={linkUrl.link}>{linkUrl.name}</NavLink>
+  </div>
+  ));
+
 const Navbar = () => {
   return (
     <nav className={style.nav}>
-      <div className={`${style.item} ${style.active}`}>
-        <NavLink to="/my/src/components/Profile/Profile.js">Profile</NavLink>
-      </div>
-
-      <div className={`${style.item} ${style.active}`}>
-        <NavLink to="/my/src/components/Dialogs/Dialogs.js">Messages</NavLink>
-      </div>
-      
-      <div className={`${style.item} ${style.active}`}>
-        <NavLink to="/my/src/components/News/News.js">News</NavLink>
-      </div>
-
-      <div className={`${style.item} ${style.active}`}>
-        <NavLink to="/my/src/components/Music/Music.js">Music</NavLink>
-      </div>
-
-      <div className={`${style.item} ${style.active}`}>
-        <NavLink to="/my/src/components/Settings/Settings.js">Settings</NavLink>
-      </div>
+      {NavLinks}
     </nav>
   );
 };
